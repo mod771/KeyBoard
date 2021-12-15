137e92feb4f8d7d940fdd4c27c119e4f47a1d753
mod771 <70249026+mod771@users.noreply.github.com>
2021-12-15 12:43:53 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,118 @@
+var key1 = document.getElementsByClassName("white-keys")[0];
+var key2 = document.getElementsByClassName("white-keys")[1];
+var key3 = document.getElementsByClassName("white-keys")[2];
+var key4 = document.getElementsByClassName("white-keys")[3];
+var key5 = document.getElementsByClassName("white-keys")[4];
+var key6 = document.getElementsByClassName("white-keys")[5];
+var key7 = document.getElementsByClassName("white-keys")[6];
+var key8 = document.getElementsByClassName("white-keys")[7];
+var key9 = document.getElementsByClassName("white-keys")[8];
+var key10 = document.getElementsByClassName("white-keys")[9];
+var key11 = document.getElementsByClassName("white-keys")[10];
+var key12 = document.getElementsByClassName("white-keys")[11];
+var key13 = document.getElementsByClassName("white-keys")[12];
+
+var key14 = document.getElementsByClassName("black-keys")[0];
+var key15 = document.getElementsByClassName("black-keys")[1];
+var key16 = document.getElementsByClassName("black-keys")[2];
+var key17 = document.getElementsByClassName("black-keys")[3];
+var key18 = document.getElementsByClassName("black-keys")[4];
+var key19 = document.getElementsByClassName("black-keys")[5];
+var key20 = document.getElementsByClassName("black-keys")[6];
+var key21 = document.getElementsByClassName("black-keys")[7];
+var key22 = document.getElementsByClassName("black-keys")[8];
+var key23 = document.getElementsByClassName("black-keys")[9];
+
+const sound1 = new Audio("audio/key-1.mp3");
+const sound2 = new Audio("audio/key-2.mp3");
+const sound3 = new Audio("audio/key-3.mp3");
+const sound4 = new Audio("audio/key-4.mp3");
+const sound5 = new Audio("audio/key-5.mp3");
+const sound6 = new Audio("audio/key-6.mp3");
+const sound7 = new Audio("audio/key-7.mp3");
+const sound8 = new Audio("audio/key-8.mp3");
+const sound9 = new Audio("audio/key-9.mp3");
+const sound10 = new Audio("audio/key-10.mp3");
+const sound11 = new Audio("audio/key-11.mp3");
+const sound12 = new Audio("audio/key-12.mp3");
+const sound13 = new Audio("audio/key-13.mp3");
+const sound14 = new Audio("audio/key-14.mp3");
+const sound15 = new Audio("audio/key-15.mp3");
+const sound16 = new Audio("audio/key-16.mp3");
+const sound17 = new Audio("audio/key-17.mp3");
+const sound18 = new Audio("audio/key-18.mp3");
+const sound19 = new Audio("audio/key-19.mp3");
+const sound20 = new Audio("audio/key-20.mp3");
+const sound21 = new Audio("audio/key-21.mp3");
+const sound22 = new Audio("audio/key-22.mp3");
+const sound23 = new Audio("audio/key-23.mp3");
+
+key1.addEventListener("click", () => {
+    sound1.play();
+})
+key2.addEventListener("click", () => {
+    sound2.play();
+})
+key3.addEventListener("click", () => {
+    sound3.play();
+})
+key4.addEventListener("click", () => {
+    sound4.play();
+})
+key5.addEventListener("click", () => {
+    sound5.play();
+})
+key6.addEventListener("click", () => {
+    sound6.play();
+})
+key7.addEventListener("click", () => {
+    sound7.play();
+})
+key8.addEventListener("click", () => {
+    sound8.play();
+})
+key9.addEventListener("click", () => {
+    sound9.play();
+})
+key10.addEventListener("click", () => {
+    sound10.play();
+})
+key11.addEventListener("click", () => {
+    sound11.play();
+})
+key12.addEventListener("click", () => {
+    sound12.play();
+})
+key13.addEventListener("click", () => {
+    sound13.play();
+})
+key14.addEventListener("click", () => {
+    sound14.play();
+})
+key15.addEventListener("click", () => {
+    sound15.play();
+})
+key16.addEventListener("click", () => {
+    sound16.play();
+})
+key17.addEventListener("click", () => {
+    sound17.play();
+})
+key18.addEventListener("click", () => {
+    sound18.play();
+})
+key19.addEventListener("click", () => {
+    sound19.play();
+})
+key20.addEventListener("click", () => {
+    sound20.play();
+})
+key21.addEventListener("click", () => {
+    sound21.play();
+})
+key22.addEventListener("click", () => {
+    sound22.play();
+})
+key23.addEventListener("click", () => {
+    sound23.play();
+})
